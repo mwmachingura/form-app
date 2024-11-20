d0481817620a02be6b24c266857f8f5d4c236b8c
Mutsa W. Machingura <mutsamaching@gmail.com>
2024-11-20 12:07:32 +0200

    Tailwind Color and height extended, base structure of site, font import

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1 className="p-6">Testing</h1></main>
+    <main className="h-screen w-screen flex justify-center items-center">
+      <div className="w-3/4 h-5/6 shadow-lg rounded-2xl flex flex-row bg-white items-center">
+        <div className="h-19/20 w-1/3 rounded-2xl bg-blue-300 m-4">
+          <p className="text-sm">Kindly note this is ubuntu 16</p>
+        </div>
+        <div className="bg-green-300 h-19/20 w-2/3 m-4"></div>
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Marineblue: "hsl(213, 96%, 18%)",
+        Purplishblue: "hsl(243, 100%, 62%)",
+        Pastelblue: "hsl(228, 100%, 84%)",
+        Lightblue: "hsl(206, 94%, 87%)",
+        Strawberryred: "hsl(354, 84%, 57%)",
+        Coolgray: "hsl(231, 11%, 63%)",
+        Lightgray: "hsl(229, 24%, 87%)",
+        Magnolia: "hsl(217, 100%, 97%)",
+        Alabaster: "hsl(231, 100%, 99%)",
+        White: "hsl(0, 0%, 100%)",
       },
+      height: {
+        "19\/20": "95%",
+      },
+      fontFamily: {
+        sans: ['var(--font-ubuntu)'],
+      }
     },
   },
   plugins: [],
